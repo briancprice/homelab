2dafc49e954fc94859da020be1f199619178ee76
Brian Price <1766482+briancprice@users.noreply.github.com>
2025-01-17 20:44:06 -0700

    Added flathub remote

--- settings/flatpak.nix
+++ settings/flatpak.nix
@@ -1,9 +1,24 @@
+# Installs flatpak support, 
+#   - Adds the gnome-software store
+#   - Adds the flathub repository
 { config, pkgs, ... }:
 {
+  # Enable flatpak
   services.flatpak.enable = true;
 
+  # Add the gnome-software store
   environment.systemPackages = with pkgs; [
     gnome-software
   ];
+
+  # Add the flathub repo
+  systemd.services.add-flatpak-repos = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
 }
 
